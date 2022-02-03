62be46d7dfaa155a0e9f7d3cb29f15247978aec9
sheshank407 <sheshankthota@gmail.com>
2022-02-03 22:07:09 +0530

    initialCommit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerRange = 1;
+            int upperRange = 100000;
+            string filename = @"E:\ConsoleTest\Testes.txt";
+            Random r = new Random();
+            int number = 0;
+                Console.WriteLine(number);
+
+            using (StreamWriter writer = new StreamWriter(filename, false, Encoding.UTF8))
+            {
+                
+                for (int i = 1; i < 10; i++)
+                {
+                    number = r.Next(lowerRange, upperRange);
+
+                    writer.WriteLine(number + ",");
+
+                }
+               // writer.Flush();
+                writer.Close();
+            }
+        }
+    }
+}
